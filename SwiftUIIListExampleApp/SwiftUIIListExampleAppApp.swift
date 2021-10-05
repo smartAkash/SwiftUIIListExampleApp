a67c26bbdfdc1642c8d92fd842cdc4cbae34fd3a
Akash <akash.nara@smartsensesolutions.com>
2021-10-05 10:08:10 +0530

    Initial Commit

---
+++ SwiftUIIListExampleApp/SwiftUIIListExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIListExampleAppApp.swift
+//  SwiftUIIListExampleApp
+//
+//  Created by Akash on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIListExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
