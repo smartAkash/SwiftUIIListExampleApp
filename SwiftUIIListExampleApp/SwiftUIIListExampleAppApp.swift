cb6269fc11417c4696fe7bea23cbda8197a4743b
Akash <akash.nara@smartsensesolutions.com>
2021-10-13 11:09:16 +0530

    added support of selected index from out side

--- SwiftUIIListExampleApp/SwiftUIIListExampleAppApp.swift
+++ SwiftUIIListExampleApp/SwiftUIIListExampleAppApp.swift
@@ -12,8 +12,10 @@ struct SwiftUIIListExampleAppApp: App {
     var body: some Scene {
         WindowGroup {
                 let arrayOfSegment = [CommonDataModel.init(title: "Jobs"),
-                                      CommonDataModel.init(title: "Search", icon: "searchAtLandingPage", iconPosition: .left),
+                                      CommonDataModel.init(title: "Search", icon: "searchAtLandingPage", iconPosition: .left, isSelected: true),
                                       CommonDataModel.init(title: "Connect", icon: "umbagog", iconPosition: .right),
+//                                      CommonDataModel.init(title: "Connect", icon: "umbagog", iconPosition: .right),
+
                 ]
                 CustomSegmentedPickerView(buttonTitlesArray: arrayOfSegment,tabHeight: 50, buttonTapAction: { dataModel in
                     debugPrint(dataModel.title)
@@ -21,4 +23,18 @@ struct SwiftUIIListExampleAppApp: App {
         }
     }
 
+}
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
 }
--- SwiftUIIListExampleApp/Views/CustomSegmentView.swift
+++ SwiftUIIListExampleApp/Views/CustomSegmentView.swift
@@ -15,45 +15,60 @@ struct CommonDataModel{
     var title:String
     var icon:String? = nil
     var iconPosition: EnumIconPosition = .none
+    var isSelected:Bool? = nil
 }
 
 struct CustomSegmentedPickerView: View {
     
-    var buttonTitlesArray: [CommonDataModel] = [CommonDataModel]() // array of title pass from out side
-    var tabHeight: CGFloat = 0 // segment hieght
+    var buttonTitlesArray: [CommonDataModel] = [CommonDataModel]() // This one for array of buttons to show in segmentview
+    var tabHeight: CGFloat = 0 // This one used for set height of main segment container view
     let buttonTapAction: (CommonDataModel) -> Void
     
+    // To get intial selected button index if we passed from out side
+    private var getIndex: Int {
+        var selectedRowIndex = 0
+        if let index = self.buttonTitlesArray.firstIndex(where: {$0.isSelected == true}){
+            selectedRowIndex = index
+        }
+        return selectedRowIndex
+    }
     var body: some View {
         VStack(alignment: .center, spacing: 0, content: {
-            configureCustomButtonSegment(buttonTitlesArray: buttonTitlesArray, frames: Array<CGRect>(repeating: .zero, count: buttonTitlesArray.count), buttonTapAction: { buttonSelectedEnum in
+            ConfigureCustomSegment(buttonTitlesArray: buttonTitlesArray, selectedIndex: getIndex,frames: Array<CGRect>(repeating: .zero, count: buttonTitlesArray.count), buttonTapAction: { buttonSelectedEnum in
                 buttonTapAction(buttonSelectedEnum)
             }).padding(.all, 5).background(Color.white)
         }).cornerRadius(tabHeight/2).padding(.leading, 20).padding(.trailing, 20).animation(.easeInOut)
     }
 }
 
-
-// Configure Segment of Button
-struct configureCustomButtonSegment: View {
+// Configure SegmentView Outer of buttons
+struct ConfigureCustomSegment: View {
     
-    var buttonTitlesArray: [CommonDataModel] = [CommonDataModel]()
-    @State var selectedIndex = 0
+    var buttonTitlesArray: [CommonDataModel] = [CommonDataModel]() // This one for array of buttons to show in segmentview
+    @State var selectedIndex = 0 // This used for store selected button index
     @State var frames: [CGRect] = [CGRect]()
-    @State private var selectedIndexBackgroundColor = Color.blue
-    var buttonTapAction: (CommonDataModel) -> Void
+    @State private var selectedIndexBackgroundColor = Color.blue // This will used for to set background color of button Capsule overlay and title color of button
+    var buttonTapAction: (CommonDataModel) -> Void // This one used for when child clouser will call clouser to pass parent view to pass currecnt selected array of index
     
     var body: some View {
         ZStack{
             HStack(spacing: 0) {
                 ForEach(Array(self.buttonTitlesArray.enumerated()), id: \.offset) { offset, item in
                     let buttonIndex = offset
-                    CustomButtonSegment1(buttonIndex: buttonIndex, selectedDataModel: item, isSelectedButton: self.selectedIndex   == buttonIndex) { (selectedIndex) in
+                    
+                    // Here we are prepared button and passing 3 things param
+                    //1. button index
+                    //2. data model,
+                    //3. is selected that button or not
+                    PreparedButtonForSegmentView(buttonIndex: buttonIndex, selectedDataModel: item, isSelectedButton: self.selectedIndex   == buttonIndex) { (selectedIndex) in
                         self.selectedIndex = selectedIndex
                         self.buttonTapAction(self.buttonTitlesArray[selectedIndex])
-                    }.foregroundColor(selectedIndexBackgroundColor).padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 10)).background(
-                        GeometryReader { geo in
-                            Color.clear.onAppear { self.setFrame(index: buttonIndex, frame: geo.frame(in: .global)) }
-                        }
+                    }
+                    .foregroundColor(selectedIndexBackgroundColor)
+                    .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 10))
+                    .background(
+                        // here we have called GeometryReader to set or store frame of current button in array of frames
+                        GeometryReader { geo in Color.clear.onAppear { self.setFrame(index: buttonIndex, frame: geo.frame(in: .global)) }}
                     )
                 }
             }
@@ -67,67 +82,40 @@ struct configureCustomButtonSegment: View {
         )
     }
     
+    // to store current buttons frame in array of frame we are storing becouse when get selected button to help us when we will add Capsule beckground at that this array of frame will needs actully buttons frame height and width
     private func setFrame(index: Int, frame: CGRect) {
         self.frames[index] = frame
     }
 }
 
-struct CustomButtonSegment1: View {
+// Button configure for segmentView
+struct PreparedButtonForSegmentView: View {
     
-    var buttonIndex = 0
-    var selectedDataModel: CommonDataModel
-    var isSelectedButton:Bool = false
-    var buttonAction: (Int) -> Void
-    private let iconSize:CGFloat = 15
+    var buttonIndex = 0 // this array of index
+    var selectedDataModel: CommonDataModel // this one for selected data model from where we will get info like title, icon, left,right support icon or not
+    var isSelectedButton: Bool = false // this one to track current button is selected or not based on that will update color, title color, background color.
+    var buttonActionClouser: (Int) -> Void // This one used for when button clicked will call clouser to pass parent view to pass currecnt selected array of index
+    private let iconSize: CGFloat = 15 // This one for fix size width and height for icon display left or right
+    private let lineLimit: Int = 1 // This one for fix button text limit of line
     
     var body : some View {
         Button(action: {
-            self.buttonAction(buttonIndex)
+            self.buttonActionClouser(buttonIndex)
         }){
+            // Here we are check icon support possition left, right or none
             switch selectedDataModel.iconPosition {
             case .none:
-                Text(selectedDataModel.title).foregroundColor(isSelectedButton ? Color.white : Color.blue)
+                Text(selectedDataModel.title).foregroundColor(isSelectedButton ? Color.white : Color.blue).lineLimit(lineLimit)
             case .left:
                 if let iconName = selectedDataModel.icon, !iconName.isEmpty{
                     Image(iconName).resizable().colorMultiply(isSelectedButton ? Color.white : Color.blue).frame(width: iconSize, height: iconSize, alignment: .center)
                 }
-                Text(selectedDataModel.title).foregroundColor(isSelectedButton ? Color.white : Color.blue)
+                Text(selectedDataModel.title).foregroundColor(isSelectedButton ? Color.white : Color.blue).lineLimit(lineLimit)
             case .right:
-                Text(selectedDataModel.title).foregroundColor(isSelectedButton ? Color.white : Color.blue)
+                Text(selectedDataModel.title).foregroundColor(isSelectedButton ? Color.white : Color.blue).lineLimit(lineLimit)
                 if let iconName = selectedDataModel.icon, !iconName.isEmpty{
                     Image(iconName).resizable().colorMultiply(isSelectedButton ? Color.white : Color.blue).frame(width: iconSize, height: iconSize, alignment: .center)
                 }
-            }
-        }
-    }
-}
-
-// Configure Button
-struct CustomButtonSegment: View {
-    
-    var buttonIndex = 0
-    var leftIcon:Bool?
-    var rightIcon:Bool?
-    var title = ""
-    var icon:String?
-    var isSelectedButton:Bool = false
-    var buttonAction: (Int) -> Void
-    private let iconSize:CGFloat = 15
-    
-    var body : some View {
-        Button(action: {
-            self.buttonAction(buttonIndex)
-        }){
-            if leftIcon == nil && rightIcon == nil {
-                Text(title).foregroundColor(isSelectedButton ? Color.white : Color.blue)
-            }else if leftIcon != nil, let iconName = icon{
-                Image(iconName).resizable().colorMultiply(isSelectedButton ? Color.white : Color.blue).frame(width: iconSize, height: iconSize, alignment: .center)
-                Text(title).foregroundColor(isSelectedButton ? Color.white : Color.blue)
-            }else if rightIcon != nil, let iconName = icon{
-                Text(title).foregroundColor(isSelectedButton ? Color.white : Color.blue)
-                Image(iconName).resizable().colorMultiply(isSelectedButton ? Color.white : Color.blue).frame(width: iconSize, height: iconSize, alignment: .center)
-            }else{
-                Text(title).foregroundColor(isSelectedButton ? Color.white : Color.blue)
             }
         }
     }
